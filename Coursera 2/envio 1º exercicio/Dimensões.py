4916092f64af6f28eccef71fcd1b1be2a8176461
R-3030casa <67975153+R-3030casa@users.noreply.github.com>
2020-08-22 18:46:53 -0300

    atualizar

--- Coursera 2/envio 1º exercicio/Dimensões.py
+++
@@ -1,8 +0,0 @@
-def dimensoes(matriz):
-    tam_matriz = (len(matriz), len(matriz[0]))
-    print('{}X{}'.format(tam_matriz[0], tam_matriz[1]))
-
-
-minha_matriz = [[1], [2], [3]]
-dimensoes(minha_matriz)
-
